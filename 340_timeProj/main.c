c5fb4a8defb78a0a562b0339dac2a15a83c4a734
yasinehsan <yasinehsan11@gmail.com>
2020-02-26 13:13:50 -0500

    Initial Commit

---
+++ 340_timeProj/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  340_timeProj
+//
+//  Created by Yasin Ehsan on 2/26/20.
+//  Copyright © 2020 Yasin Ehsan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
